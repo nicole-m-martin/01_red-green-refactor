d24305d118db09ef021d1752bfb25e7121755bfb
Nicole Martin <n.martinpdx@gmail.com>
2021-03-22 18:25:24 -0700

    captalizeAndFilter refactor to map and pass

--- capitalizeAndFilterFunction/capitalizeAndFilter.js
+++ capitalizeAndFilterFunction/capitalizeAndFilter.js
@@ -1,8 +1,16 @@
+// const capitalizeAndFilter = (word) => {
+//   for (let i = 0; i < word.length; i++) {
+//     word[i] = word[i].toUpperCase();
+//   }
+//   return word.filter((fItem) => fItem.charAt(0) !== 'F');
+// };
+
+// module.exports = capitalizeAndFilter;
+
 const capitalizeAndFilter = (word) => {
-  for (let i = 0; i < word.length; i++) {
-    word[i] = word[i].toUpperCase();
-  }
-  return word.filter((fItem) => fItem.charAt(0) !== 'F');
+  return word
+    .map((fItem) => fItem.toUpperCase())
+    .filter((fItem) => fItem.charAt(0) !== 'F');
 };
 
 module.exports = capitalizeAndFilter;
